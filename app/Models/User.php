65440eb0b3030251f883792aaef8dcf773911247
Naveen <naveenproplus222@gmail.com>
2026-06-15 11:46:14 +0530

    Site Task

--- app/Models/User.php
+++ app/Models/User.php
@@ -113,9 +113,7 @@ class User extends Authenticatable
 
     public function tasks()
     {
-        return ProjectTask::whereHas('project.site.supervisors', function ($query) {
-            $query->where('users.id', $this->id);
-        });
+        return ProjectTask::forSupervisor($this->id);
     }
     public function devices(): HasMany
     {
